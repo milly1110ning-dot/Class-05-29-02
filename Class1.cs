78d72e144645865a5e9a604f3e9b9806129ff44c
milly1110ning-dot <milly.1110ning@gmail.com>
2026-05-29 10:39:35 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public abstract class Person
+{
+	public virtual string GetName()
+	{
+		return "Person";
+	}
+} 
+public class Student :Person
+{
+	public sealed override string GetName()
+	{
+		return "Student";
+	}
+}
+public class Teacher : Student
+{
+    public override string GetName()
+    {
+        return "Student";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Class_05_29_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Student();
+            Console.WriteLine(p1.GetName());
+        }
+    }
+}
